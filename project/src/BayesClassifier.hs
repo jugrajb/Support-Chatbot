eff97f5a6ef89c9acc4eb027d4286a2a0deecad5
wmatous <willmatous@gmail.com>
2019-10-14 14:21:29 -0700

    trained classifier

---
+++ project/src/BayesClassifier.hs
@@ -0,0 +1,35 @@
+     module BayesClassifier where
+    -- Text Classifier Using Bayes Formula
+    import Data.List
+    import Data.Char
+    type Category = String
+    newtype Classifier = Classifier { training :: [(Category, [String])] } deriving (Eq, Show)
+    -- Get a new classifer with no training
+    classifier :: Classifier
+    classifier = Classifier []
+    -- classifier probabilities
+    probabilityOfWordInCategory :: Classifier -> String -> Category -> Double
+    probabilityOfCategory :: Classifier -> Category -> Double
+    -- Adding + 1 for Laplacian Correction
+    probabilityOfWordInCategory (Classifier training) word category = let allInCategory = filter (\(cat, _) -> cat == category) training
+                                                                          allInCategoryContainingWord = filter (\(_, text) -> word `elem` text) allInCategory
+                                                                      in (fromIntegral $ length allInCategoryContainingWord + 1) / (fromIntegral $ length allInCategory + 1)
+    probabilityOfCategory (Classifier training) category =  let allInCategory = filter (\(cat, _) -> cat == category) training 
+                                                            in (fromIntegral $ length allInCategory) / (fromIntegral $ length training)
+    -- Train a classifier
+    train :: Classifier -> String -> Category -> Classifier 
+    train (Classifier training ) text category = Classifier $ (category, cleanInput $ text):training
+    -- Categorize text with a classifier
+    classify :: Classifier -> String -> Category
+    classify classifier text = fst $ head $ sortBy (\(_, a) (_, b)  -> b `compare` a) $ probabilities classifier text
+    -- Get Probability for each Category
+    probabilities :: Classifier ->  String -> [(Category, Double)] 
+    probabilities classifier@(Classifier training) text =  map (\cat -> (cat, probabilityForCategory classifier text cat)) $ nub $ map (\(cat, _) -> cat) training
+    -- Get Probability for a passage in a certain category
+    probabilityForCategory :: Classifier -> String -> Category -> Double
+    probabilityForCategory classifier text category = (+) (log $ probabilityOfCategory classifier category)  (sum $ map (\word -> log $ probabilityOfWordInCategory classifier word category) $ cleanInput text) 
+    -- Lowercase, Remove Punctuation
+    cleanInput :: String -> [String]
+    cleanInput text = filter (\w -> not (w `elem` stopWords)) $ words $ filter (`elem` ' ':['a'..'z']) $ map toLower text 
+                      where stopWords = ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours ","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"]
+   
---
+++ project/src/ChatClassifier.hs
@@ -0,0 +1,69 @@
+  
+
+import System.IO
+import BayesClassifier
+
+splitsep :: (a -> Bool) -> [a] -> [[a]]
+splitsep _ [] = []
+splitsep s l = (fst fs):(splitsep s (snd fs))
+    where fs = firstsep s [] l
+
+firstsep:: (a -> Bool) -> [a] -> [a] -> ([a], [a])
+firstsep _ p [] = (p,[])
+firstsep s p (h:t) 
+    | s h = (p, t)
+    | otherwise = firstsep s (p++[h]) t
+
+
+splitcsv :: FilePath -> IO [[String]]
+splitcsv filename =
+    do
+    filecontent <- readFile filename
+    let lines = splitsep (=='\r') filecontent
+    let lines_split = [splitsep (==',') line | line <- lines]
+    return lines_split
+
+
+trainline:: Classifier -> [String] -> Classifier
+trainline cls a = train cls (a !! 0) (a !! 1)
+
+
+trainedClassifier :: IO Classifier
+trainedClassifier = do
+    trainingData <- splitcsv "htraining.csv"
+    let trained_classifier = foldl trainline classifier trainingData
+    return trained_classifier
+
+{--
+interactionLoop myClassifier function = case function of 
+                                            "start" ->  
+                                                do
+                                                    putStrLn "Enter an action [train|classify]"
+                                                    action <- getLine
+                                                    interactionLoop myClassifier action
+                                            "train" -> 
+                                                do
+                                                    putStr "Category: "
+                                                    category <- getLine
+                                                    putStr "Material: "
+                                                    material <- getLine
+                                                    interactionLoop (train myClassifier material category) "start"
+                                            "classify" ->
+                                                do
+                                                    putStr "Material: "
+                                                    material <- getLine
+                                                    putStrLn $ classify myClassifier material
+                                                    --putStrLn . show $ probabilities myClassifier material
+                                                    --putStrLn "\n\n\n\n"
+                                                    interactionLoop myClassifier "start"
+                                            _ ->
+                                                    interactionLoop myClassifier "start"
+main = do
+        trainingData <- splitcsv "htraining.csv"
+        hSetBuffering stdout NoBuffering
+        hSetBuffering stdin NoBuffering
+        trained_classifier = foldl trainline classifier trainingData
+
+
+--}
+        
