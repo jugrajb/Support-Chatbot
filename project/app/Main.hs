23bce5a6916b5be486cadf08ef047a2888a53e39
wmatous <willmatous@gmail.com>
2019-10-15 16:47:36 -0700

    question loop

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -36,38 +36,42 @@ module Main where
         hdl <- socketToHandle sock ReadWriteMode
         chat_classifier <- trainedClassifier "./src/chatbot-training-data.csv"
         hSetBuffering hdl NoBuffering
-        hPutStrLn hdl "Hi, what do you need assistance with?"
-        question <- fmap init (hGetLine hdl)
-        let category = classify chat_classifier question
-        -- get top response for category
-        -- hPutStrLn hdl (getTopResponse category)
+        
 
-        hPutStrLn hdl "Was this what you were looking for? (y/n)"
-        answer <- fmap init (hGetLine hdl)
-        if answer == "y"
-          then 
-            -- interactionloop (bumpProbability category response)
-            -- increase choice probability ( if not top choice)
-          else
-            -- show next answer
-        -- hPutStrLn hdl (classify chat_classifier text)
-    
-        commLine <- dupChan chan
-    
-        -- fork off a thread for reading from the duplicated channel
-        reader <- forkIO $ fix $ \loop -> do
-            (nextNum, line) <- readChan commLine
-            when (msgNum /= nextNum) $ hPutStrLn hdl line
-            loop
-    
-        handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
-            line <- fmap init (hGetLine hdl)
-            case line of
-                 -- If an exception is caught, send a message and break the loop
-                 "quit" -> hPutStrLn hdl "Bye!"
-                 -- else, continue looping.
-                 --_      -> broadcast (name ++ ": " ++ line) >> loop
-    
-        killThread reader                      -- kill after the loop ends
-        --broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
-        hClose hdl                             -- close the handle
+
+interactionLoop chat_classifier probabilities = do
+  hPutStrLn hdl "What do you need assistance with?"
+  question <- fmap init (hGetLine hdl)
+  let category = classify chat_classifier question
+  -- get top response for category
+  -- hPutStrLn hdl (getTopResponse probabilities category)
+
+  hPutStrLn hdl "Was this what you were looking for? (y/n)"
+  answer <- fmap init (hGetLine hdl)
+  if answer == "y"
+    then 
+      -- interactionloop chat_classifier (bumpProbability category response)
+      -- increase choice probability ( if not top choice)
+    else
+      -- show next answer
+      -- hPutStrLn hdl (classify chat_classifier text)
+
+  commLine <- dupChan chan
+
+  -- fork off a thread for reading from the duplicated channel
+  reader <- forkIO $ fix $ \loop -> do
+      (nextNum, line) <- readChan commLine
+      when (msgNum /= nextNum) $ hPutStrLn hdl line
+      loop
+
+  handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+      line <- fmap init (hGetLine hdl)
+      case line of
+           -- If an exception is caught, send a message and break the loop
+           "quit" -> hPutStrLn hdl "Bye!"
+           -- else, continue looping.
+           --_      -> broadcast (name ++ ": " ++ line) >> loop
+
+  killThread reader                      -- kill after the loop ends
+  --broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
+  hClose hdl                             -- close the handle
