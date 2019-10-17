517f810804925d05500ec2b8103fc4547a21ce5b
Jugraj Bath <jugraj.bath@gmail.com>
2019-10-17 10:10:18 -0700

    Finsihed answer selection

--- project/app/ChatClassifier.hs
+++ project/app/ChatClassifier.hs
@@ -5,66 +5,28 @@ import System.IO
 import BayesClassifier
 
 splitsep :: (a -> Bool) -> [a] -> [[a]]
-splitsep _ [] = []
-splitsep s l = (fst fs):(splitsep s (snd fs))
-    where fs = firstsep s [] l
+splitsep s = foldr (\e (h:t) -> if s e then []:(h:t) else (e:h):t) [[]] 
 
-firstsep:: (a -> Bool) -> [a] -> [a] -> ([a], [a])
-firstsep _ p [] = (p,[])
-firstsep s p (h:t) 
-    | s h = (p, t)
-    | otherwise = firstsep s (p++[h]) t
+readCsv :: FilePath -> IO [[String]]
+readCsv filename = do
+    file <- readFile filename
+    return [splitsep (==',') row | row <- ( splitsep (=='\n') file )]
 
-
-splitcsv :: FilePath -> IO [[String]]
-splitcsv filename =
-    do
-    filecontent <- readFile filename
-    let lines = splitsep (=='\r') filecontent
-    let lines_split = [splitsep (==',') line | line <- lines]
-    return lines_split
-
+fetchAnswer :: FilePath -> Category -> IO String
+fetchAnswer filename c = do
+    file <- readFile filename
+    let list =  [splitsep (==',') row | row <- ( splitsep (=='\n') file )]
+    let search = [r | (h:r) <- list, h == c]
+    let resp = search !! 0 !! 0
+    return resp
 
 trainline:: Classifier -> [String] -> Classifier
 trainline cls a = train cls (a !! 0) (a !! 1)
 
-
 trainedClassifier :: FilePath -> IO Classifier
 trainedClassifier fp = do
-    trainingData <- splitcsv fp
+    trainingData <- readCsv fp
     let trained_classifier = foldl trainline classifier trainingData
     return trained_classifier
 
-{--
-interactionLoop myClassifier function = case function of 
-                                            "start" ->  
-                                                do
-                                                    putStrLn "Enter an action [train|classify]"
-                                                    action <- getLine
-                                                    interactionLoop myClassifier action
-                                            "train" -> 
-                                                do
-                                                    putStr "Category: "
-                                                    category <- getLine
-                                                    putStr "Material: "
-                                                    material <- getLine
-                                                    interactionLoop (train myClassifier material category) "start"
-                                            "classify" ->
-                                                do
-                                                    putStr "Material: "
-                                                    material <- getLine
-                                                    putStrLn $ classify myClassifier material
-                                                    --putStrLn . show $ probabilities myClassifier material
-                                                    --putStrLn "\n\n\n\n"
-                                                    interactionLoop myClassifier "start"
-                                            _ ->
-                                                    interactionLoop myClassifier "start"
-main = do
-        trainingData <- splitcsv "htraining.csv"
-        hSetBuffering stdout NoBuffering
-        hSetBuffering stdin NoBuffering
-        trained_classifier = foldl trainline classifier trainingData
-
-
---}
         
--- project/app/Main.hs
+++ project/app/Main.hs
@@ -1,77 +1,76 @@
--- Main.hs, final code
 module Main where
 
-    import Network.Socket
-    import System.IO
-    import Control.Exception
-    import Control.Concurrent
-    import Control.Monad (when)
-    import Control.Monad.Fix (fix)
-    import ChatClassifier
-    import BayesClassifier
-    
-    main :: IO ()
-    main = do
-      sock <- socket AF_INET Stream 0
-      setSocketOption sock ReuseAddr 1
-      bind sock (SockAddrInet 4242 iNADDR_ANY)
-      listen sock 2
-      chan <- newChan
-      _ <- forkIO $ fix $ \loop -> do
-        (_, _) <- readChan chan
-        loop
-      mainLoop sock chan 0
-    
-    type Msg = (Int, String)
-    
-    mainLoop :: Socket -> Chan Msg -> Int -> IO ()
-    mainLoop sock chan msgNum = do
-      conn <- accept sock
-      forkIO (runConn conn chan msgNum)
-      mainLoop sock chan $! msgNum + 1
-    
-    runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
-    runConn (sock, _) chan msgNum = do
-        let broadcast msg = writeChan chan (msgNum, msg)
-        hdl <- socketToHandle sock ReadWriteMode
-        chat_classifier <- trainedClassifier "./src/chatbot-training-data.csv"
-        hSetBuffering hdl NoBuffering
-        
-
-
-interactionLoop chat_classifier probabilities = do
-  hPutStrLn hdl "What do you need assistance with?"
-  question <- fmap init (hGetLine hdl)
-  let category = classify chat_classifier question
-  -- get top response for category
-  -- hPutStrLn hdl (getTopResponse probabilities category)
-
-  hPutStrLn hdl "Was this what you were looking for? (y/n)"
-  answer <- fmap init (hGetLine hdl)
-  if answer == "y"
-    then 
-      -- interactionloop chat_classifier (bumpProbability category response)
-      -- increase choice probability ( if not top choice)
-    else
-      -- show next answer
-      -- hPutStrLn hdl (classify chat_classifier text)
-
-  commLine <- dupChan chan
-
-  -- fork off a thread for reading from the duplicated channel
-  reader <- forkIO $ fix $ \loop -> do
-      (nextNum, line) <- readChan commLine
-      when (msgNum /= nextNum) $ hPutStrLn hdl line
+  import Network.Socket
+  import System.IO
+  import System.Directory
+  import Control.Exception
+  import Control.Concurrent
+  import Control.Monad (when)
+  import Control.Monad.Fix (fix)
+  import ChatClassifier
+  import BayesClassifier
+  
+  main :: IO ()
+  main = do
+    sock <- socket AF_INET Stream 0
+    setSocketOption sock ReuseAddr 1
+    bind sock (SockAddrInet 4241 iNADDR_ANY)
+    listen sock 2
+    chan <- newChan
+    _ <- forkIO $ fix $ \loop -> do
+      (_, _) <- readChan chan
       loop
+    mainLoop sock chan 0
+  
+  type Msg = (Int, String)
+  
+  mainLoop :: Socket -> Chan Msg -> Int -> IO ()
+  mainLoop sock chan msgNum = do
+    conn <- accept sock
+    forkIO (runConn conn chan msgNum)
+    mainLoop sock chan $! msgNum + 1
+  
+  runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
+  runConn (sock, _) chan msgNum = do
+      let broadcast msg = writeChan chan (msgNum, msg)
+      hdl <- socketToHandle sock ReadWriteMode
+      curdDir <- getCurrentDirectory
+      chat_classifier <- trainedClassifier (curdDir ++ "/project/src/questions.csv")
+      hSetBuffering hdl NoBuffering
 
-  handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
-      line <- fmap init (hGetLine hdl)
-      case line of
-           -- If an exception is caught, send a message and break the loop
-           "quit" -> hPutStrLn hdl "Bye!"
-           -- else, continue looping.
-           --_      -> broadcast (name ++ ": " ++ line) >> loop
+      -- Initial Message
+      hPutStrLn hdl "Chatbot: Hello, what did you need assistance with?"
 
-  killThread reader                      -- kill after the loop ends
-  --broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
-  hClose hdl                             -- close the handle
+      -- Message Loop
+      handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
+          question <- fmap init (hGetLine hdl)
+          case question of
+               -- If an exception is caught, send a message and break the loop
+               "quit" -> hPutStrLn hdl "Chatbot: Goodbye!"
+               -- else, continue looping.
+               _ -> do
+                  hPutStrLn hdl ("User: " ++ question)
+
+                  -- get answer
+                  let category = classify chat_classifier question
+                  hPutStrLn hdl ("Category: " ++ category)
+
+                  let prob = probabilityForCategory chat_classifier question category
+                  let ansDir = (curdDir ++ "/project/src/answers.csv")
+                  str <- fetchAnswer ansDir (dropWhile (==' ') category)
+
+                  hPutStrLn hdl ("Prob: " ++ show prob)
+                  hPutStrLn hdl ("Chatbot: " ++ str)
+
+                  hPutStrLn hdl "Chatbot: To help us improve, was this answer helpful? (y/n)"
+                  answer <- fmap init (hGetLine hdl)
+                  if answer == "y"
+                    then 
+                      --- if answer is good, add question with category to questions.csv
+                      hPutStrLn hdl "Chatbot: If you need anymore help feel free to ask another question, otherwise type 'quit' to exit"
+                    else
+                      -- nothing
+                      hPutStrLn hdl "Chatbot: Sorry, please ask the question again for a more relevant answer"
+
+                  loop
+      hClose hdl                             -- close the handle
